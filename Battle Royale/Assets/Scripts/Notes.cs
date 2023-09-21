0a739c586b780e0d9241517b1fad48de864c4253
dahmanp <dahmanp@my.erau.edu>
2023-09-20 19:07:40 -0700

    Add Project

---
+++ Battle Royale/Assets/Scripts/Notes.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    //When starting to work on this again, start on page 43
+    //TO FIX: Scene loading is not working, figure that out at some point lol
+}
