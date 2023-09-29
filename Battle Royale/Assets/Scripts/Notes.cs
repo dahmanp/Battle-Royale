10525a574f99c23257a17254eb599f5d29fac7bf
dahmanp <dahmanp@my.erau.edu>
2023-09-28 17:17:23 -0700

    idk

--- Battle Royale/Assets/Scripts/Notes.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Notes : MonoBehaviour
-{
-    //When starting to work on this again, start on page 43
-    //TO FIX: Scene loading is not working, figure that out at some point lol
-}
